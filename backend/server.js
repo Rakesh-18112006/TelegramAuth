94592bfe431066a8b2ce78540b760ba0f027333d
Rakesh <mummanarakesh@gmail.com>
2025-03-18 18:09:31 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "*", methods: "GET,POST" }));
 
 // Telegram Auth Route - Handles GET requests from the Telegram widget
 app.get("/auth", (req, res) => {
